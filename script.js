b743d82ab84055d3349ef139b18e101d1710b151
Ajay Kumar <112938234+ajaykr2712@users.noreply.github.com>
2024-05-11 23:18:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Get the menu button element and the navigation menu element
-const btnMenu = document.querySelector('.menu-button');
-const navMenu = document.querySelector('.navigation-menu');
+// Selecting the menu button and navigation menu elements
+const menuButton = document.querySelector('.menu-button');
+const navigationMenu = document.querySelector('.navigation-menu');
 
-// Add a click event listener to the menu button
-btnMenu.addEventListener('click', function() {
-  // Toggle the visibility of the navigation menu
-  navMenu.classList.toggle('show-menu');
+// Adding a click event listener to the menu button
+menuButton.addEventListener('click', function() {
+  // Toggling the visibility of the navigation menu
+  navigationMenu.classList.toggle('show-menu');
 });
